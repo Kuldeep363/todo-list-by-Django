02ad7fec8f130cbbbc0320a8f95a9bf552855982
KuldeepRawat363 <Kuldeep363@users.UI.github.com>
2020-05-13 11:43:03 +0530

    settings changed

--- todo/settings.py
+++ todo/settings.py
@@ -1,6 +1,6 @@
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -12,11 +12,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ql@qlpt#tr91_$6jig&$1$ljita(ldc%qwboa@kt%74)8vv0zv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kr-todo.herokuapp.com']
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -108,3 +108,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
